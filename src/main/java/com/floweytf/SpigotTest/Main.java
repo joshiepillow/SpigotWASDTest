672cbb62df97163adbfebeb71ffebf355da2f9fd
Joshiepillow <joshbce123@gmail.com>
2023-05-03 20:02:37 -0500

    broken code but need to save

--- src/main/java/com/floweytf/SpigotTest/Main.java
+++ src/main/java/com/floweytf/SpigotTest/Main.java
@@ -1,5 +1,10 @@
 package com.floweytf.SpigotTest;
 
+import com.comphenix.protocol.PacketType;
+import com.comphenix.protocol.ProtocolLibrary;
+import com.comphenix.protocol.events.PacketAdapter;
+import com.comphenix.protocol.events.PacketEvent;
+import com.comphenix.protocol.reflect.FieldAccessException;
 import com.floweytf.SpigotTest.commands.Test;
 import com.floweytf.SpigotTest.commands.TestStop;
 import org.bukkit.plugin.java.JavaPlugin;
@@ -9,6 +14,21 @@ public class Main extends JavaPlugin {
     public void onEnable() {
         this.getCommand("test").setExecutor(new Test());
         this.getCommand("teststop").setExecutor(new TestStop());
+        ProtocolLibrary.getProtocolManager().addPacketListener(new PacketAdapter(this, PacketType.Play.Client.STEER_VEHICLE){
+            @Override
+            public void onPacketReceiving(PacketEvent event) {
+                if (event.isCancelled()) return;
+                try {
+                    float swSpeed = event.getPacket().getFloat().read(0);
+                    float fwSpeed = event.getPacket().getFloat().read(1);
+                    Test.update(event.getPlayer(), swSpeed, fwSpeed);
+
+                } catch (FieldAccessException ex){
+                    ex.printStackTrace();
+                    return; // if something goes wrong, we exit and don't do anything with the packet.
+                }
+            }
+        });
     }
 
     @Override
--- src/main/java/com/floweytf/SpigotTest/commands/Test.java
+++ src/main/java/com/floweytf/SpigotTest/commands/Test.java
@@ -1,40 +1,56 @@
 package com.floweytf.SpigotTest.commands;
 
 import com.floweytf.SpigotTest.Main;
+import de.tr7zw.nbtapi.NBTEntity;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.Location;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.EntityType;
+import org.bukkit.entity.Minecart;
 import org.bukkit.entity.Player;
 import org.bukkit.util.Vector;
 import org.javatuples.Pair;
 
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.UUID;
 
 public class Test implements CommandExecutor {
-    public static HashMap<UUID, Pair<Integer, String>> hashMap = new HashMap<>();
+    private enum Direction {
+        UP, DOWN, LEFT, RIGHT, NONE
+    }
+    private class DataStructure {
+        DataStructure(int nD, Direction lD, HashMap<Direction, Integer> lP)  {
+            noteDelay = nD;
+            lastDirection = lD;
+            lastPressWASD = lP;
+        }
+        public int noteDelay;
+        public Direction lastDirection;
+        public HashMap<Direction, Integer> lastPressWASD;
+    }
+    public static HashMap<UUID, DataStructure> hashMap = new HashMap<>();
     public static List<String> correctSequence = List.of("+X", "+X", "+Z", "+Z");
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
         try {
             Player psender = (Player) sender;
             int delayTicks = Integer.parseInt(args[0]);
-            Location l = psender.getLocation().setDirection(new Vector(0, 0, 1));
+            Location l = psender.getLocation().setDirection(new Vector(1, 0, 0));
             psender.teleport(l);
+            psender.
             int id = Bukkit.getScheduler().scheduleSyncRepeatingTask(Main.getPlugin(Main.class), () -> {
-                Player psender2 = Bukkit.getServer().getPlayer(psender.getUniqueId());
-                if (psender2 == null) {
+                Player player = Bukkit.getServer().getPlayer(psender.getUniqueId());
+                if (player == null) { // If player is not online
                     Bukkit.getScheduler().cancelTask(hashMap.get(psender.getUniqueId()).getValue0());
                     hashMap.remove(psender.getUniqueId());
                     return;
                 }
-                psender2.sendTitle(ChatColor.DARK_RED + "Test", null, 0, delayTicks, 0);
-                Vector v = psender2.getLocation().subtract(l).toVector();
+                player.sendTitle(ChatColor.DARK_RED + "Test", null, 0, delayTicks, 0);
+                Vector v = player.getLocation().subtract(l).toVector();
                 String s = "";
                 if (!v.isZero()) {
                     if (v.getX() + v.getZ() > 0) {
@@ -51,13 +67,13 @@ public class Test implements CommandExecutor {
                             s = "-X";
                         }
                     }
-                    if (!hashMap.get(psender2.getUniqueId()).getValue1().equals(s)) {
-                        psender2.sendMessage(s);
+                    if (!hashMap.get(player.getUniqueId()).getValue1().equals(s)) {
+                        player.sendMessage(s);
                     }
-                    psender2.teleport(l);
+                    player.teleport(l);
 
                 }
-                hashMap.put(psender2.getUniqueId(), new Pair<>(hashMap.get(psender2.getUniqueId()).getValue0(), s));
+                hashMap.put(player.getUniqueId(), new Pair<>(hashMap.get(player.getUniqueId()).getValue0(), s));
             }, 0, delayTicks);
             hashMap.put(psender.getUniqueId(), new Pair<>(id, ""));
         } catch (Exception e) {
@@ -68,5 +84,25 @@ public class Test implements CommandExecutor {
     }
 
 
+    void setup(Player p, int noteDelay) {
+        Location l = p.getLocation().setDirection(new Vector(1, 0, 0));
+        p.teleport(l);
+        Minecart m = (Minecart) p.getWorld().spawnEntity(l, EntityType.MINECART);
+        NBTEntity e = new NBTEntity(m);
+        e.setByte("NoAI", (byte) 1);
+        m.addPassenger(p);
 
+        hashMap.put(p.getUniqueId(), new DataStructure(noteDelay, Direction.NONE, new HashMap<>()));
+    }
+
+    void loop(int n, Player p) {
+
+
+    }
+
+    public static void update(Player p, float swSpeed, float fwSpeed) {
+        if (!hashMap.containsKey(p.getUniqueId())) return;
+        if (fwSpeed > swSpeed)
+
+    }
 }
